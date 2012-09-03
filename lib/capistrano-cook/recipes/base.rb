cc99e652a4fc5452e2c72fcc812fea889158634b
Mark <mark.pietrucha@gmail.com>
2012-09-03 17:32:00 +0200

    testing copying ssh key to remote host

--- lib/capistrano-cook/recipes/base.rb
+++ lib/capistrano-cook/recipes/base.rb
@@ -1,6 +1,14 @@
 require "digest"
 
 Capistrano::Configuration.instance.load do
+  set_default(:copy_ssh_key, true)
+  set_default(:ssh_key_name) do
+    Capistrano::CLI.ui.ask "Provide name for the rsa file (default is 'id', come up with with smth more meaningful): "
+  end
+  set_default(:ssh_key_passphrase) do
+    Capistrano::CLI.ui.ask "Yo dude, this shit is important! Type a strong passphrase: "
+  end
+
   def template(from, to)
     if File.exists?("deploy/templates/#{from}")
       logger.info "using template form #{File.absolute_path('deploy/template/' + from)}"
@@ -18,7 +26,6 @@ Capistrano::Configuration.instance.load do
   def generate_password(len=16)
     Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{user}--")[0,len]
   end
-
 
   namespace :deploy do
     desc "Install everything on server"
@@ -53,5 +60,49 @@ Capistrano::Configuration.instance.load do
       run "rm tmp_pass"
       set :user, base_user
     end
+
+    task :copy_ssh_key do
+      if copy_ssh_key 
+
+        begin
+          run "mkdir ~/.ssh"
+        rescue Capistrano::CommandError => e
+          logger.info ".ssh directory already exists in the home directory of #{user}"
+        end
+
+        id_rsa_name     = copy_ssh_key
+        rsa_passphrase  = ssh_key_passphrase
+
+        id_rsa_name ||= 'id_rsa'
+
+        begin
+          existing_file = system("ls -x1 ~/.ssh/#{id_rsa_name}.pub")
+        rescue Capistrano::CommandError => e
+          logger.info "File does not exist, need to generate one"
+          existing_file = false
+        end
+
+        if existing_file == false
+          rsa_passphrase && rsa_passphrase.length > 0 ? 
+            run("ssh-keygen -f ~/.ssh/#{id_rsa_name} -N #{rsa_passphrase}") : 
+            run("ssh-keygen -f ~/.ssh/#{id_rsa_name}")
+          existing_file = "~/.ssh/#{id_rsa_name}.pub"
+        end
+
+        if existing_file != false
+          key = system("cat ~/.ssh/#{id_rsa_name}.pub")
+          if key && key.length > 0
+            begin
+              run "mkdir ~/.ssh"
+            rescue Capistrano::CommandError => e
+              logger.info "remote .ssh directory already exists"
+            end
+            run "echo '#{key}' >> ~/.ssh/authorized_keys"
+          end
+        end
+
+      end
+    end
+    after "root:add_user", "root:copy_ssh_key"
   end
 end
